a265481c324bec02e4e07b97f6f56d0b2524887b
GoodGopnik <https://github.com/GoodGopnik>
2021-07-16 16:21:37 +0300

    commit

--- main.js
+++ main.js
@@ -7,10 +7,30 @@ $('.multiple-items').slick({
     nextArrow: "<div class='next-arrow'>",
 });
   $('.conditions-items').slick({
+    centerMode: true,
+    centerPadding: '0',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: false,
     prevArrow: "<div class='prev-arrow'>",
     nextArrow: "<div class='next-arrow'>",
+});
+$('.home-img-items').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: "<div class='prev-arrow'>",
+    nextArrow: "<div class='next-arrow'>",
+    responsive: [
+        {
+          breakpoint: 1440,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+      ]
 });
