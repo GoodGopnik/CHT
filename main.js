291b0572c1e58f905eb87fd492974319cc143940
GoodGopnik <https://github.com/GoodGopnik>
2021-07-16 12:55:09 +0300

    Первая страница

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    prevArrow: "<div class='prev-arrow'>",
+    nextArrow: "<div class='next-arrow'>",
+  });
